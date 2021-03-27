511acf1d0ab307e3457c280b91cb808a2245bdbc
kcoms555 <kcoms555@naver.com>
2021-03-27 15:15:54 +0900

    refactor: add dbconnector

--- backend/webserver/dao/users.js
+++ backend/webserver/dao/users.js
@@ -1,19 +1,6 @@
-const mysql = require('mysql2');
-
 class Users {}
 
-//mysql setting. 'mysql2' is used for async mysql interfaces.
-//비동기적으로 DB 연결이 수립되기에 Object.freeze() 는 연결이 수립된 이후에 호출한다.
-(async () => {
-    const db__ = await mysql.createPool({
-        host: process.env['DBHOST'],
-        user: process.env['DBID'],
-        password: process.env['DBPW'],
-        database: 'food_manager'
-    });
-    Users.db = await db__.promise();
-    Object.freeze(Users);
-})();
+require('../utils/dbconnector')(Users);
 
 Users.getUser = async (userId, password) => {
     const [row, fields] = await Users.db.execute("SELECT * FROM users WHERE user_id=? AND password=? LIMIT 1", [userId, password]);
