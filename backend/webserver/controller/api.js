01000a54e8cdac2cbf4364ae139a9602d4936420
jdrae <dr.kiwi.j@gmail.com>
2021-03-30 00:40:08 +0900

    refactor: placeholder in dao & wrapper in controller

--- backend/webserver/controller/api.js
+++ backend/webserver/controller/api.js
@@ -18,13 +18,9 @@ router.post('/logout', fctl.loginRequiredWrapper(async (req, res, next) => {
     return fctl.send(req, res, hsc.HTTP_OK, null);
 }));
 
-router.post('/recommend', async (req, res)=>{
-    try{
-        data = await service.recommendRecipes(req.body.userIngredients);
-        return fctl.send(req, res, hsc.HTTP_OK, data);
-    } catch (error){
-        return fctl.send(req, res, hsc.HTTP_INTERNAL_SERVER_ERROR, {"message": error});
-    }
-});
+router.post('/recommend', fctl.nonLoginWrapper(async (req, res, next) => {
+    data = await service.recommendRecipes(req.body.userIngredients);
+    return fctl.send(req, res, hsc.HTTP_OK, data);
+}));
 
 module.exports = router;
--- backend/webserver/dao/recipes.js
+++ backend/webserver/dao/recipes.js
@@ -3,11 +3,9 @@ class Recipes {}
 require('../utils/dbconnector')(Recipes);
 
 Recipes.getRecipes = async (recipeIds) => {
-    const [row, fields] = await Recipes.db.execute("SELECT * FROM recipes WHERE id IN ("+recipeIds+")");
-    if (row.length === 0) {
-        return null;
-    }
-    return {'result': row};
+    const [row, fields] = await Recipes.db.execute("SELECT * FROM recipes WHERE FIND_IN_SET(id, ?)", [recipeIds]);
+
+    return {'result': row}; // 검색 결과가 없을 경우 row 는 빈 리스트 []
 }
 
 module.exports = Recipes;
