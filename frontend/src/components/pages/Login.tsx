55b889192c5af9bac91f5a1dffaedad41c4ab8ff
hyun gyu <gusrb3164@gmail.com>
2021-03-30 00:56:50 +0900

    feat: login, signup page 디자인 개선

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,7 +1,20 @@
 import AcUnitIcon from "@material-ui/icons/AcUnit";
-import { InputField, SubmitButton } from "../index";
+import { InputField } from "../index";
+import { Link } from "react-router-dom";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        width: "80%",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        color: "#ffffff",
+        background: "linear-gradient(45deg, #00d6af 30%, #00c3cf 90%)"
+    }
+});
 
 const Login = () => {
+    const classes = useStyles();
     return (
         <div className="login-container">
             <div className="login-head">
@@ -14,9 +27,16 @@ const Login = () => {
                 <InputField text="PW" type="password" />
             </div>
             <div className="login-submit">
-                <SubmitButton />
-                <div>Forgot ID / PW ?</div>
-                <div>Don't have account? REGISTER</div>
+                <Button variant="contained" color="primary" className={classes.root}>
+                    Login
+                </Button>
+                {/* <div>Forgot ID / PW ?</div> */}
+                <div className="login-register">
+                    Don't have account?&nbsp;
+                    <Link to="/signup" className="login-link">
+                        REGISTER
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/pages/SignUp.tsx
+++ frontend/src/components/pages/SignUp.tsx
@@ -1,7 +1,20 @@
 import AcUnitIcon from "@material-ui/icons/AcUnit";
 import { InputField, SubmitButton } from "../index";
+import { Link } from "react-router-dom";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        width: "80%",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        color: "#ffffff",
+        background: "linear-gradient(45deg, #00d6af 30%, #00c3cf 90%)"
+    }
+});
 
 const SignUp = () => {
+    const classes = useStyles();
     return (
         <div className="login-container">
             <div className="login-head">
@@ -15,8 +28,15 @@ const SignUp = () => {
                 <InputField text="PW Correction" type="password" />
             </div>
             <div className="login-submit">
-                <SubmitButton />
-                <div>I already have my account SIGN IN</div>
+                <Button variant="contained" color="primary" className={classes.root}>
+                    Submit
+                </Button>
+                <div className="login-register">
+                    I already have my account&nbsp;
+                    <Link to="/login" className="login-link">
+                        SIGN IN
+                    </Link>
+                </div>
             </div>
         </div>
     );
