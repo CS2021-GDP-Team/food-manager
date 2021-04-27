b47c42585e499413236820de9d45724791686c3a
hyun gyu <gusrb3164@gmail.com>
2021-04-27 15:32:04 +0900

    fix: add fridge_menu

--- frontend/src/components/pages/Picture.tsx
+++ frontend/src/components/pages/Picture.tsx
@@ -39,7 +39,7 @@ const Picture = () => {
         console.log(putDate);
         await axios
             .post("/food-manager/api/user_fridge", {
-                ingredientId: 99,
+                ingredientId: 137,
                 expireDate: new Date(expDate).getTime() / 1000,
                 putDate: new Date(putDate).getTime() / 1000
             })
