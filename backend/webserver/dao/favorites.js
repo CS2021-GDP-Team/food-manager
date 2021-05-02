28d26dadf0735afb7201c62e17fa3453f7a8047c
jdrae <dr.kiwi.j@gmail.com>
2021-05-02 19:21:06 +0900

    fix: respond recipe name in api(diet, favorite)

--- backend/webserver/dao/favorites.js
+++ backend/webserver/dao/favorites.js
@@ -3,7 +3,8 @@ class Favorites {}
 require('../utils/dbconnector')(Favorites);
 
 Favorites.getFavoritesByUserId = async (userId) => {
-    const [row, fields] = await Favorites.db.execute("SELECT * FROM favorites WHERE user_id=?", [userId]);
+    // score = 1 인 값(좋아요) 만 가져옴
+    const [row, fields] = await Favorites.db.execute("SELECT id, name, favorite_id, score FROM recipes r JOIN ( SELECT id as favorite_id, recipe_id,score FROM favorites WHERE user_id=? AND score=1) f ON r.id = f.recipe_id", [userId]);
 	return row;
 }
 
