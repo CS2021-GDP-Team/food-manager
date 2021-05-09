08d502893506c994a0018caeea4c04b486ddb65d
kcoms555 <kcoms555@naver.com>
2021-05-09 17:26:33 +0100

    feat: create ratio column in db

--- backend/data/dbinsertor.py
+++ backend/data/dbinsertor.py
@@ -3,7 +3,7 @@ import requests
 import json
 import re
 from parse import parse
-from dbaccessor DBAccessor
+from dbaccessor import DBAccessor
 
 def fetch_items_from_file(path):
 	with open(path, 'r') as f:
@@ -15,7 +15,7 @@ db = DBAccessor()
 for item in fetch_items_from_file('result'):
 	recipe = db.get_recipe_by_name(item['name'])
 	if not recipe:
-		recipe_id = db.insert_recipe(item['name'])
+		recipe_id = db.insert_recipe(item['name'], item['url'])
 	else:
 		recipe_id = recipe['id']
 
@@ -33,5 +33,5 @@ for item in fetch_items_from_file('result'):
 		recipe_ing = db.get_recipe_ingredient_by_recipe_ingredient(recipe_id, ing_id)
 		if not recipe_ing:
 			amount = int(float(re.sub('(\d+)(g|ml)', '\\1', amounts[i])))
-			recipe_ing_id = db.insert_recipe_ingredient(recipe_id, ing_id, amount, url)
+			recipe_ing_id = db.insert_recipe_ingredient(recipe_id, ing_id, amount)
 db.commit()
