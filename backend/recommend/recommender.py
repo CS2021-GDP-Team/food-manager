69d31b64677618a92b793412a54874470aa9c91a
kcoms555 <kcoms555@naver.com>
2021-06-08 13:33:51 +0100

    fix: edit params

--- backend/recommend/recommender.py
+++ backend/recommend/recommender.py
@@ -27,8 +27,8 @@ def j_sim(uem, rem):
             if uem[iid]>0 and rem[rid,iid]>0:
                 ing_w-=(uem[iid]*rem[rid,iid]) 
             # 없을시에 레시피 재료 가중치를 더함
-            else:
-                ing_w+=rem[rid,iid]
+            #else:
+            #    ing_w+=rem[rid,iid]
         ing_w_list.append(ing_w)
     return np.array(ing_w_list).argsort()
 
